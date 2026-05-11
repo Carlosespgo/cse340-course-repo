78ea0504f8d5de7c71d3b11b5d68d1877825f3d1
carlosespgo <cebc2224@gmail.com>
2026-05-11 10:26:25 -0500

    add categories

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cse340-course-repo
+
+# Default hosting: https://cse340ce.onrender.com
--- server.js
+++ server.js
@@ -39,6 +39,11 @@ app.get('/organizations', async (req, res) => {
     res.render('organizations', { title });
 });
 
+app.get('/categories', async (req, res) => {
+    const title = 'Categories';
+    res.render('categories', { title });
+});
+
 app.get('/projects', async (req, res) => {
     const title = 'Service Projects';
     res.render('projects', { title });
